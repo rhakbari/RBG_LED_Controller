e5d85d6360f6e7612b12a23228fc3e83faa2f57b
Adeel Hasan Akbari <adeel.akbari@gmail.com>
2020-09-21 16:18:24 -0400

    Format using black.
    see: https://pypi.org/project/black/

--- effectThread.py
+++ effectThread.py
@@ -2,17 +2,17 @@ import sys
 import threading
 import time
 
+
 class StoppableThread(threading.Thread):
-    
     def __init__(self):
         super(StoppableThread, self).__init__()
         self._stopper = threading.Event()
 
     def stopit(self):
-	try:
-        	self._stopper.set()
-	except Error:
-		print "NOTHING TO STOP"
+        try:
+            self._stopper.set()
+        except Error:
+            print "NOTHING TO STOP"
 
     def stopped(self):
         return self._stopper.is_set()
--- server.py
+++ server.py
@@ -25,7 +25,7 @@ led_socket.listen(5)
 print "LED SERVER STARTED AT " + SERVER_IP + " ON PORT: " + str(SERVER_PORT)
 
 oEffectThread = effect_cop(1)
-oFadeThread = ColorFader(1,R_CURRENT,G_CURRENT,B_CURRENT,0,0,0)
+oFadeThread = ColorFader(1, R_CURRENT, G_CURRENT, B_CURRENT, 0, 0, 0)
 oFadeThread.start()
 
 read_list = [led_socket]
@@ -37,55 +37,54 @@ while True:
             read_list.append(client_socket)
         else:
             byteData = s.recv(24)
-	    data = byteData.decode('utf-8')
+            data = byteData.decode("utf-8")
             if len(data) > 0:
-		if data == "OFF":
-			oEffectThread.stopit()
-			setRGB(0,0,0)
-			print "rgb off"
-  
+                if data == "OFF":
+                    oEffectThread.stopit()
+                    setRGB(0, 0, 0)
+                    print "rgb off"
+
                 if data == "CLOFF":
-                        GPIO.output(31,GPIO.HIGH)
-                        print "CLOFF "
-               
+                    GPIO.output(31, GPIO.HIGH)
+                    print "CLOFF "
+
                 if data == "CLON":
-                        GPIO.output(31,GPIO.LOW)
-                        print "CLON  "
-                        
-		if data == "STOP":
-			oEffectThread.stopit()
+                    GPIO.output(31, GPIO.LOW)
+                    print "CLON  "
 
-		if data == "COPS":
-			oEffectThread.stopit()
-			oEffectThread = effect_cop(0.5)
-			oEffectThread.start()
+                if data == "STOP":
+                    oEffectThread.stopit()
 
-		if data == "FADE":
-			oEffectThread.stopit()
-			oEffectThread = effect_fade(0.03)
-			oEffectThread.start()
-              
-			
-                aRGB = data.split(',')
+                if data == "COPS":
+                    oEffectThread.stopit()
+                    oEffectThread = effect_cop(0.5)
+                    oEffectThread.start()
+
+                if data == "FADE":
+                    oEffectThread.stopit()
+                    oEffectThread = effect_fade(0.03)
+                    oEffectThread.start()
+
+                aRGB = data.split(",")
                 if len(aRGB) == 3:
-			oEffectThread.stopit()
+                    oEffectThread.stopit()
 
-                        r = int(aRGB[0])
-                        g = int(aRGB[1])
-                        b = int(aRGB[2])			
+                    r = int(aRGB[0])
+                    g = int(aRGB[1])
+                    b = int(aRGB[2])
 
-			if MODE is "fade":
-				oFadeThread.stopit()
-				oFadeThread = ColorFader(1000,R_CURRENT,G_CURRENT,B_CURRENT,r,g,b)
-				oFadeThread.start()
-				#BUFFER VALUES TO FADE FROM THERE NEXT TIME
-				R_CURRENT = r
-				G_CURRENT = g
-				B_CURRENT = b
-			else:
-				setRGB(r,g,b)
+                    if MODE is "fade":
+                        oFadeThread.stopit()
+                        oFadeThread = ColorFader(
+                            1000, R_CURRENT, G_CURRENT, B_CURRENT, r, g, b
+                        )
+                        oFadeThread.start()
+                        # BUFFER VALUES TO FADE FROM THERE NEXT TIME
+                        R_CURRENT = r
+                        G_CURRENT = g
+                        B_CURRENT = b
+                    else:
+                        setRGB(r, g, b)
             else:
                 s.close()
                 read_list.remove(s)
-
-
